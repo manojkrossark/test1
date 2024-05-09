b53e7ff7a1b6d84fa9a25fba4090b87e7c853d22
manojkumar <69884509+manojkrossark@users.noreply.github.com>
2024-05-09 13:50:16 +0530

    Delete routes directory

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function index(req, res) {
-    res.send('Online');
-}
-exports.default = index;
-//# sourceMappingURL=index.js.map
